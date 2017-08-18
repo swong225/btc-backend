df02970947bade384364e9ee711cb9779e1ec35e
stephen <swong225@gmail.com>
2017-08-18 15:47:29 -0400

    Add eslint

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   // res.send('respond with a resource');
-  res.json([{
-  	id: 1,
-  	username: "testUN"
-  }, {
-  	id: 2,
-  	username: "anotherTest"
-  }]);
+  res.json([
+    {
+      id: 1,
+      username: 'testUN',
+    },
+    {
+      id: 2,
+      username: 'anotherTest',
+    },
+  ]);
 });
 
 module.exports = router;
