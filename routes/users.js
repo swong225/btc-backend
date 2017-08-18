f48d951783ae7c57d261331cca0b8496c8459707
stephen <swong225@gmail.com>
2017-08-18 17:09:54 -0400

    WIP - establish connection with db

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,30 @@
 const express = require('express');
+const { Client } = require('pg');
+
+const { POSTGRES_USER, POSTGRES_HOST, POSTGRES_DB } = require('../config');
+const { POSTGRES_PASSWORD } = require('../config/secrets');
 
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  // res.send('respond with a resource');
-  res.json([
-    {
-      id: 1,
-      username: 'testUN',
-    },
-    {
-      id: 2,
-      username: 'anotherTest',
-    },
-  ]);
+  const client = new Client({
+    user: POSTGRES_USER,
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    password: POSTGRES_PASSWORD,
+    port: 5432
+  });
+
+  client.connect();
+
+  client.query('SELECT $1::json as message', [{
+    id: 1,
+    username: 'testUN'
+  }], (err, response) => {
+    res.send([response.rows[0].message]);
+    client.end();
+  });
 });
 
 module.exports = router;
