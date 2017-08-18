443273d676235d604a6a0fa92a05ebc9bcbb4727
stephen <swong225@gmail.com>
2017-08-18 17:43:59 -0400

    Extrapolate db connection to file

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,16 @@
 const express = require('express');
-const { Client } = require('pg');
 
-const { POSTGRES_USER, POSTGRES_HOST, POSTGRES_DB } = require('../config');
-const { POSTGRES_PASSWORD } = require('../config/secrets');
+const db = require('../db');
 
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  const client = new Client({
-    user: POSTGRES_USER,
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    password: POSTGRES_PASSWORD,
-    port: 5432
-  });
-
-  client.connect();
-
-  client.query('SELECT $1::json as message', [{
+  db.query('SELECT $1::json as message', [{
     id: 1,
     username: 'testUN'
-  }], (err, response) => {
-    res.send([response.rows[0].message]);
-    client.end();
+  }]).then(result => {
+    res.send([result.rows[0].message]);
   });
 });
 
