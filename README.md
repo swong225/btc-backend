b883dfe875edbae3cae681033115cd5b1f0b54b1
stephen <swong225@gmail.com>
2017-08-19 14:39:31 -0400

    eslint changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### Setup
 Requires a postgres.env file that contains:
 
-POSTGRES_PASSWORD=set_password_here
-POSTGRES_USER=set_username_here
-POSTGRES_DB=set_db_here
+POSTGRES_PASSWORD=set_password_here  
+POSTGRES_USER=set_username_here  
+POSTGRES_DB=set_db_here  
--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,13 @@ const db = require('../db');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  db.query('SELECT $1::json as message', [{
+router.get('/', async (req, res) => {
+  const result = await db.query('SELECT $1::json as message', [{
     id: 1,
     username: 'testUN'
-  }]).then(result => {
-    res.send([result.rows[0].message]);
-  });
+  }]);
+
+  res.send([result.rows[0].message]);
 });
 
 module.exports = router;
